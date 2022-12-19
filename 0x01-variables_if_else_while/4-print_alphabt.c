08859a42d638d09ac4547ec94e8e4895a0669f3c
aishamidris <aishamuhammadidris92@gmail.com>
2022-12-18 16:46:16 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - alphabet
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char dennis, e, q;
+	e = 'e';
+	q = 'q';
+	
+	for (dennis = 'a'; dennis <= 'z'; dennis++)
+	{
+		if (dennis != e && dennis != q)
+			putchar(dennis);
+	}
+	putchar('\n');
+	return (0);
+}
