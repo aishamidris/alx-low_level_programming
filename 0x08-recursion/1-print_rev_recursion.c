1e588e9c3adb9c56dd4286e1c102b29a866d4e2e
aishamidris <aishamuhammadidris92@gmail.com>
2023-01-05 15:17:21 -0800

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: string
+ * print _rev_recursion - prints a string.
+ * followed by a new line.
+ * @s:an input string to printing.
  * Return: no return.
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_puts_recursion(s + 1);
 	}
-	else
-		_putchar('\n');
+
 }
